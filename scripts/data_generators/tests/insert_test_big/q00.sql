ecf139cd635ca4c3b83965b445f5a67bc3214c61
Yan <60644013+yan-alex@users.noreply.github.com>
2026-03-10 13:54:54 +0100

    specify WITH ('write.parquet.compression-codec'='zstd') - because some catalogs do not expose that by default -

--- scripts/data_generators/tests/insert_test_big/q00.sql
+++ scripts/data_generators/tests/insert_test_big/q00.sql
@@ -4,6 +4,6 @@ CREATE or REPLACE TABLE default.insert_test_big (
 )
 TBLPROPERTIES (
     'format-version'='2',
-    'write.update.mode'='merge-on-read'
+    'write.update.mode'='merge-on-read',
     'write.parquet.compression-codec'='zstd'
 );
